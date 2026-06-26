f28195f421cb3914742684a1c8717f0f4b6b3803
cstwtr-web <cst.wtr@gmail.com>
2026-06-26 12:50:54 +0200

    Add files via upload

--- task_15_map_interaction.js
+++ task_15_map_interaction.js
@@ -155,7 +155,7 @@ export function _applyMapView() {
     _mapState.hasBeenFitted = true;
     const _bounds = _mapState.pendingBounds;
     if (_bounds) {
-      try { map.fitBounds(_bounds, { padding: [20, 20], maxZoom: 15 }); } catch (e) {}
+      try { map.fitBounds(_bounds, { paddingTopLeft: [20, 20], paddingBottomRight: [60, 20], maxZoom: 16 }); } catch (e) {}
     }
   } else if (_mapState.focusLatLon) {
     const { lat, lon, zoom } = _mapState.focusLatLon;
